289f3f0c3101234741bfa18d614b77d18626a746
ebastien <bastien.exposito@ynov.com>
2022-10-11 17:08:42 +0200

    premiere fonction

--- main/main.go
+++ main/main.go
@@ -6,13 +6,6 @@ import (
 	"os"
 )
 
-type HangManData struct {
-	Word             string     // Word composed of '_', ex: H_ll_
-	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
-	Attempts         int        // Number of attempts left
-	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
-}
-
 func main() {
 }
 
