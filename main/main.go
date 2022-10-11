fa7c54d28ff7d1a6894c74563396bb98a84caad4
renvel <envel.righini@ynov.com>
2022-10-11 17:25:01 +0200

    2

--- main/main.go
+++ main/main.go
@@ -1,12 +1,23 @@
 package main
 
-type HangManData struct {
-	Word             string     // Word composed of '_', ex: H_ll_
-	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
-	Attempts         int        // Number of attempts left
-	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
-}
+import (
+	"bufio"
+	"math/rand"
+	"os"
+)
 
 func main() {
-
+}
+func RandomWord(words []string) string {
+	return words[rand.Intn(len(words))]
+}
+func ReadFileLineByLine(fileName string) []string {
+	file, _ := os.Open(fileName)
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	fmtprintln.(lines)
+	return lines
 }
