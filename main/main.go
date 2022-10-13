d9205a58fe56762314a051b03f8b69c9048ca182
renvel <envel.righini@ynov.com>
2022-10-13 15:06:43 +0200

    ajou pendu

--- main/main.go
+++ main/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	min := 1
+	max := 12
+	fmt.Println(rand.Intn(max-min+1) + min)
 }
+
 func RandomWord(words []string) string {
 	return words[rand.Intn(len(words))]
 }
+
 func ReadFileLineByLine(fileName string) []string {
 	file, _ := os.Open(fileName)
 	scanner := bufio.NewScanner(file)
@@ -18,6 +26,5 @@ func ReadFileLineByLine(fileName string) []string {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-	fmtprintln.(lines)
 	return lines
 }
