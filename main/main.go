317fb05ab190b9a8509ef2062a61733a33f693cd
ebastien <bastien.exposito@ynov.com>
2022-10-17 14:38:55 +0200

    random

--- main/main.go
+++ main/main.go
@@ -12,14 +12,18 @@ func main() {
 	fmt.Println(RandomWord(ligneparligne("words.txt")))
 }
 
-func random() {
+func random(n int) int {
 	y1 := rand.NewSource(time.Now().UnixNano())
 	x1 := rand.New(y1)
-	fmt.Println(x1)
+	return x1.Intn(n)
 }
 
-func RandomWord(words []string) string {
-	return words[rand.Intn(len(words))]
+// func RandomWord(words []string) string {
+// 	return words[random(len(words))]
+// }
+
+func RandomWord2(words2 []string) string {
+	return words2[random(len(words2))]
 }
 
 func ligneparligne(fileName string) []string {
