f6641376f53678dedce44deb6c95e499e925a255
renvel <envel.righini@ynov.com>
2022-10-17 15:34:40 +0200

    debut affichage

--- main/main.go
+++ main/main.go
@@ -5,14 +5,22 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
+	fmt.Println("Good Luck, you have 10 attempts.")
 	fmt.Println(RandomWord(linebyline("words.txt")))
-	// r := rand.Intn(3) + 1
 }
+
+func random(n int) int {
+	y1 := rand.NewSource(time.Now().UnixNano())
+	x1 := rand.New(y1)
+	return x1.Intn(n)
+}
+
 func RandomWord(words []string) string {
-	return words[rand.Intn(len(words))]
+	return words[random(len(words))]
 }
 func linebyline(fileName string) []string {
 	file, _ := os.Open(fileName)
