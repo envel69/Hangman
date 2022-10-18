df2c7f5e0f9f132851fa5cce0a66880948e21a8d
ebastien <bastien.exposito@ynov.com>
2022-10-18 08:33:21 +0200

    user_app

--- main/main.go
+++ main/main.go
@@ -8,24 +8,25 @@ import (
 	"time"
 )
 
-func main() {
-	RW := RandomWord
-
-	fmt.Println(underscore(RW(ligneparligne("words.txt"))))
+func main() { //fonction main
+	RW := RandomWord //racourci de RandomWord pour simplifier
+	//fmt.Println(RW(ligneparligne("words.txt")))//marche pas je sais pas pk voir avec envel!!!!!
+	fmt.Println(user_app())                                 //a trouvé pk ca marche pas PUTIN!!!
+	fmt.Println(underscore(RW(ligneparligne("words.txt")))) // ecriture du mot en tiret
 
 }
 
-func random(n int) int {
+func random(n int) int { //fonciont random avec le temp en second
 	y1 := rand.NewSource(time.Now().UnixNano())
 	x1 := rand.New(y1)
 	return x1.Intn(n)
 }
 
-func RandomWord(words []string) string {
+func RandomWord(words []string) string { //liste de mot
 	return words[random(len(words))]
 }
 
-func ligneparligne(fileName string) []string {
+func ligneparligne(fileName string) []string { //liste des mot ligne par ligne
 	file, _ := os.Open(fileName)
 	scanner := bufio.NewScanner(file)
 	lines := []string{}
@@ -35,11 +36,18 @@ func ligneparligne(fileName string) []string {
 	return lines
 }
 
-func underscore(RW string) string {
+func underscore(RW string) string { // boucle pour transformé le mot en tiret
 	var u string
 	//for i := 0; i < len(RW); i++ {
 	for range RW {
 		u += "_"
 	}
 	return u
+}
+
+func user_app() string {
+	var lettre string
+	fmt.Println("choisie la lettre: ")
+	fmt.Scanln(&lettre)
+	return lettre
 }
