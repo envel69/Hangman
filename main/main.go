f4615b4228b06f33a07a73bd37a561d2fd9b11bf
renvel <envel.righini@ynov.com>
2022-10-18 16:44:09 +0200

    testfuncremplacement

--- main/main.go
+++ main/main.go
@@ -11,22 +11,22 @@ import (
 func main() {
 	RW := RandomWord
 	fmt.Println("Good Luck, you have 10 attempts.")
-	fmt.Println(underscore(RW(ligneparligne("words.txt"))))
+	fmt.Println(Underscore(RW(Ligneparligne("../words.txt"))))
 	//fmt.Println(RandomWord(ligneparligne("words.txt")))
-	fmt.Println(user_app())
+	fmt.Println(User_app())
 }
 
-func random(n int) int {
+func Random(n int) int {
 	y1 := rand.NewSource(time.Now().UnixNano())
 	x1 := rand.New(y1)
 	return x1.Intn(n)
 }
 
 func RandomWord(words []string) string {
-	return words[random(len(words))]
+	return words[Random(len(words))]
 }
 
-func underscore(RW string) string {
+func Underscore(RW string) string {
 	var u string
 	fmt.Println(RW)
 
@@ -36,18 +36,34 @@ func underscore(RW string) string {
 	return u
 }
 
-func ligneparligne(fileName string) []string {
+func Ligneparligne(fileName string) []string {
 	file, _ := os.Open(fileName)
 	scanner := bufio.NewScanner(file)
 	lines := []string{}
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
+	//fmt.Println(lines)
 	return lines
 }
 
-func user_app() string {
+func User_app() string {
 	var lettre string
 	fmt.Scanln(&lettre)
 	return lettre
+}
+
+type HangManData struct {
+	Word             string     // Word composed of '_', ex: H_ll_
+	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
+	Attempts         int        // Number of attempts left
+	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+}
+
+func Remplacement(input, string, ToFind, ModifiedWord) string {
+	for i := 0; i < len(input); i++ {
+		if ToFind != ModifiedWord {
+			i++
+		}
+	}
 }
