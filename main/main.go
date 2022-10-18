2b389c7652ac32d235ae58dbdd193c1a02061c9c
renvel <envel.righini@ynov.com>
2022-10-18 09:09:21 +0200

    user_app/

--- main/main.go
+++ main/main.go
@@ -9,8 +9,11 @@ import (
 )
 
 func main() {
+	RW := RandomWord
 	fmt.Println("Good Luck, you have 10 attempts.")
-	fmt.Println(RandomWord(linebyline("words.txt")))
+	fmt.Println(underscore(RW(ligneparligne("words.txt"))))
+	//fmt.Println(RandomWord(ligneparligne("words.txt")))
+	fmt.Println(user_app())
 }
 
 func random(n int) int {
@@ -22,7 +25,18 @@ func random(n int) int {
 func RandomWord(words []string) string {
 	return words[random(len(words))]
 }
-func linebyline(fileName string) []string {
+
+func underscore(RW string) string {
+	var u string
+	fmt.Println(RW)
+
+	for range RW {
+		u += "_ "
+	}
+	return u
+}
+
+func ligneparligne(fileName string) []string {
 	file, _ := os.Open(fileName)
 	scanner := bufio.NewScanner(file)
 	lines := []string{}
@@ -30,4 +44,10 @@ func linebyline(fileName string) []string {
 		lines = append(lines, scanner.Text())
 	}
 	return lines
+}
+
+func user_app() string {
+	var lettre string
+	fmt.Scanln(&lettre)
+	return lettre
 }
